cc1b44d3186290d2cdbea7357b2475b11b7dfdc9
Frankie Fong <frankiefonghk@yahoo.com>
2020-02-22 23:03:01 +0800

    Added 'helmet' to the middleware processing.

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var compression = require('compression');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog');
+var helmet = require('helmet');
 
 var app = express();
 
@@ -21,6 +22,7 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(helmet());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
